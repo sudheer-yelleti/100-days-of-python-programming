bd7f621abada70cfc0f1e87689960c6dd2f74e81
sudheer <sudheer.167@gmail.com>
2025-10-19 20:28:46 -0700

    Updated the code for Day 1 tasks.
    Signed-off-by: sudheer <sudheer.167@gmail.com>

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,4 @@
-input("What is your name?")
-
+print("Welcome to the Band Name Generator!")
+cityName = input("What is the city you grew up?\n")
+petName = input("What is your pet's name?\n")
+print("Your band name could be " + cityName + " " + petName)
