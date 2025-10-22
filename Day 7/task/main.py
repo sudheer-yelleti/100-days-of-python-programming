e69bfdd8572c740c6cdbca900ac985d191b98660
sudheer <sudheer.167@gmail.com>
2025-10-22 12:33:08 -0700

    Updated the code for Day 6,7 tasks.
    Signed-off-by: sudheer <sudheer.167@gmail.com>

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,57 @@
-word_list = ["aardvark", "baboon", "camel"]
+import random
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+import hangman_art
+import hangman_words
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+word_list = hangman_words.word_list
+lives = 6
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+print(hangman_art.logo)
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+guessed_letters = []
+while not game_over:
+
+    print(f"****************************<???>{lives}/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in guessed_letters:
+        print(f"You have already guessed {guess}")
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        if guess not in guessed_letters:
+            guessed_letters.append(guess)
+            lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        if lives == 0:
+            game_over = True
+
+            print(f"IT WAS {chosen_word}!YOU LOSE")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(hangman_art.stages[lives])
