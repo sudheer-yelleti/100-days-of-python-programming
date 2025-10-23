37a3b3f987e209b941d3f48722241d81f8075040
sudheer <sudheer.167@gmail.com>
2025-10-22 22:18:03 -0700

    Updated the code for Day 8 tasks.
    Signed-off-by: sudheer <sudheer.167@gmail.com>

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -5,13 +5,22 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+# Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt(original_text, shift_amount):
+    # Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+    # by the shift amount and print the encrypted text.
+    refractor_text = ""
+    for letter in original_text:
+        index = alphabet.index(letter) + shift_amount
+        if index > len(alphabet):
+            index = index - len(alphabet)
+        refractor_text += alphabet[index]
+    print(refractor_text)
 
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
 
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+# What happens if you try to shift z forwards by 9? Can you fix the code?
 
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
+# Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
+# message.
 
+encrypt(text, shift)
---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,7 @@
+def greet():
+    print("Hi")
+    print("Hello")
+    print("How are you?")
+
+
+greet()
