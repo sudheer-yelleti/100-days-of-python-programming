51578852791ea60db56f0c6c344a82026292e8d8
sudheer <sudheer.167@gmail.com>
2025-10-23 14:50:52 -0700

    Updated the code for Day 10 tasks.
    Signed-off-by: sudheer <sudheer.167@gmail.com>

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,6 @@
 def format_name(f_name, l_name):
+    """Takes the first and last name and format
+     it to return the concatenated name in title case"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
