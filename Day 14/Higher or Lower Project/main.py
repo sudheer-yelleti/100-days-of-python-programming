0a00b6498975424298b89dd90171737edac1e197
sudheer <sudheer.167@gmail.com>
2025-10-25 17:55:17 -0700

    Formatted the code for Day 15 tasks.

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -1,6 +1,8 @@
-import art
 import random
+
+import art
 import game_data
+
 print(art.logo)
 a = random.choice(game_data.data)
 b = random.choice(game_data.data)
@@ -8,11 +10,13 @@ while a == b:
     b = random.choice(game_data.data)
 score = 0
 
+
 def check_answer(user_guess, a_followers, b_followers):
     if a_followers > b_followers:
         return user_guess == "A"
     elif b_followers > a_followers:
         return user_guess == "B"
+
 
 should_game_continue = True
 while should_game_continue:
@@ -26,16 +30,16 @@ while should_game_continue:
         continue
     is_guess_correct = check_answer(guess, a["follower_count"], b["follower_count"])
     if is_guess_correct:
-            score += 1
-            print(f"You guessed right! Current score: {score}")
-            if a["follower_count"] > b["follower_count"]:
-                winner = a
-            else:
-                winner = b
-            a = winner
-            b= random.choice(game_data.data)
-            while a == b:
-                b = random.choice(game_data.data)
+        score += 1
+        print(f"You guessed right! Current score: {score}")
+        if a["follower_count"] > b["follower_count"]:
+            winner = a
+        else:
+            winner = b
+        a = winner
+        b = random.choice(game_data.data)
+        while a == b:
+            b = random.choice(game_data.data)
     else:
-            should_game_continue = False
-            print(f"Sorry, that's wrong. Final score: {score}")
+        should_game_continue = False
+        print(f"Sorry, that's wrong. Final score: {score}")
