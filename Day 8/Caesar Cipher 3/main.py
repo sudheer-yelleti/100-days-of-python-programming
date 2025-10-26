c1824618109357e49ed3ed43aeb7c6abb5fbbcf7
sudheer <sudheer.167@gmail.com>
2025-10-25 18:00:15 -0700

    Formatted the code for Day 8 tasks.

--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -1,7 +1,9 @@
 import art
 
 print(art.logo)
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
