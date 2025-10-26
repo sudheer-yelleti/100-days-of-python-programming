802651b24dd1088b3ea3ec25b7fa987555872267
sudheer <sudheer.167@gmail.com>
2025-10-25 18:04:29 -0700

    Formatted the code for Day 15 tasks.

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -1,11 +1,14 @@
 import data
+
 profit = 0
 
+
 def report():
-  print(f"Water: {data.resources['water']}ml")
-  print(f"Milk: {data.resources['milk']}ml")
-  print(f"Coffee: {data.resources['coffee']}g")
-  print(f"Money: {profit}$")
+    print(f"Water: {data.resources['water']}ml")
+    print(f"Milk: {data.resources['milk']}ml")
+    print(f"Coffee: {data.resources['coffee']}g")
+    print(f"Money: {profit}$")
+
 
 def check_resources(drink):
     required_ingredients = drink["ingredients"]
@@ -16,47 +19,49 @@ def check_resources(drink):
             return False
     return True
 
-def process_coins():
 
+def process_coins():
     print("Please insert coins.")
-    total  = int(input("How many quarters?: ")) * 0.25
+    total = int(input("How many quarters?: ")) * 0.25
     total += int(input("How many dimes?: ")) * 0.1
     total += int(input("How many nickles?: ")) * 0.05
     total += int(input("How many pennies?: ")) * 0.01
 
     return total
 
+
 def prepare_order(user_choice):
-  drink = data.MENU[user_choice]
-  are_resources_enough =  check_resources(drink)
+    drink = data.MENU[user_choice]
+    are_resources_enough = check_resources(drink)
 
-  if are_resources_enough:
-     order_cost =  process_coins()
-     cost = drink["cost"]
-     if order_cost < cost:
-         print("Sorry, that's not enough money. Money refunded..")
-         return
+    if are_resources_enough:
+        order_cost = process_coins()
+        cost = drink["cost"]
+        if order_cost < cost:
+            print("Sorry, that's not enough money. Money refunded..")
+            return
 
-     if order_cost > cost:
-         refund_money = round(order_cost - cost,2)
-         print(f"Here is ${refund_money} dollars in change.")
+        if order_cost > cost:
+            refund_money = round(order_cost - cost, 2)
+            print(f"Here is ${refund_money} dollars in change.")
 
-     global profit
-     profit  += cost
+        global profit
+        profit += cost
 
-     for item, amount in drink["ingredients"].items():
-       data.resources[item] -= amount
+        for item, amount in drink["ingredients"].items():
+            data.resources[item] -= amount
 
-     print(f"Here is your {user_choice}. Enjoy!")
+        print(f"Here is your {user_choice}. Enjoy!")
+
 
 is_on = True
 while is_on:
- user_choice = input("What would you like? (espresso/latte/cappuccino):")
- if user_choice == "report":
-    report()
- elif user_choice == "off":
-   is_on = False
- elif user_choice in data.MENU:
-    prepare_order(user_choice)
- else:
-     print("Sorry, that's not a valid option. Please choose espresso/latte/cappuccino")
+    user_choice = input("What would you like? (espresso/latte/cappuccino):")
+    if user_choice == "report":
+        report()
+    elif user_choice == "off":
+        is_on = False
+    elif user_choice in data.MENU:
+        prepare_order(user_choice)
+    else:
+        print("Sorry, that's not a valid option. Please choose espresso/latte/cappuccino")
